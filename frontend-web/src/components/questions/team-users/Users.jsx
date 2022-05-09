cd7f2c7cf74b0a83ed5117dea5a64ba09309ed68
DeveloperFrank <67014462+Fatih90FM91@users.noreply.github.com>
2022-05-10 01:22:41 +0200

    Update Users.jsx

--- frontend-web/src/components/questions/team-users/Users.jsx
+++ frontend-web/src/components/questions/team-users/Users.jsx
@@ -15,7 +15,7 @@ export default class Users extends Component {
         console.log(currentToken);
         if(currentToken){
            
-            axiosInstance.get("http://localhost:5000/all-users")
+            axios.get("http://localhost:5000/all-users")
                 .then( response => {
                     console.log(response);
                     this.setState({ result: response.data })
