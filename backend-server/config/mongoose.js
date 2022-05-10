449933fd1277db4f9fa951ce41a29f91b55ab7e7
Fatih90FM91 <hasan9091.fs@gmail.com>
2022-05-11 01:35:17 +0200

    addindgg

--- backend-server/config/mongoose.js
+++ backend-server/config/mongoose.js
@@ -12,7 +12,7 @@
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI ||  "mongodb://localhost:27017/mern_database_frank", {
+    await mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_URI, {
       useNewUrlParser: true,
       // useFindAndModify: true,
       // useUnifiedTopology: true,
