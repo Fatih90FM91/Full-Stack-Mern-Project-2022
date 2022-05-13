9528341d748c15ee73dfbbe279174acd8ff74f42
Fatih90FM91 <hasan9091.fs@gmail.com>
2022-05-14 00:43:27 +0200

    updated access

--- server.js
+++ server.js
@@ -51,11 +51,11 @@ const PORT= process.env.PORT || 5000;
 }));
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
 });
-
 
 app.use(passport.initialize());
 app.use(passport.session());
