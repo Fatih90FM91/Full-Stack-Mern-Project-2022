5e158f7a7188b538b0cb5b32f6548cc4ed7c4747
Fatih90FM91 <hasan9091.fs@gmail.com>
2022-05-14 01:10:20 +0200

    fixed origin

--- frontend-web/src/components/Login.jsx
+++ frontend-web/src/components/Login.jsx
@@ -9,6 +9,7 @@ const devENV = process.env.NODE_ENV !== "production";
 const {REACT_APP_DEV_API  , REACT_APP_PROD_API} = process.env;
 
 console.log(REACT_APP_PROD_API)
+console.log(devENV);
 
 
 export default class Login extends Component {
